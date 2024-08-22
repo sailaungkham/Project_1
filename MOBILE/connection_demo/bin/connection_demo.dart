15a09d5eb6470406d9cb4700ca3803e21f6e1f5c
Srrt <seatsiri.d@gmail.com>
2024-08-22 11:06:25 +0700

    final

--- MOBILE/connection_demo/bin/connection_demo.dart
+++ MOBILE/connection_demo/bin/connection_demo.dart
@@ -1,102 +1,176 @@
+
+
+
+
+// Registration
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'dart:io';
 
+
 void main() async {
   await login();
 
-  Future<void> login() async {
-    print("===== Login =====");
 
-    // Get username and password
-    stdout.write("Username: ");
-    String? username = stdin.readLineSync()?.trim();
-    stdout.write("Password: ");
-    String? password = stdin.readLineSync()?.trim();
+}
+// ADD function
+  Future<void> addNewExpense(int userId) async {
+  print("===== Add New items =====");
 
-    if (username == null || password == null) {
-      print("Incomplete input");
-      return;
-    }
+  // Get item description
+  stdout.write("Item: ");
+  String? item = stdin.readLineSync()?.trim();
 
-    final body = {"username": username, "password": password};
-    final url = Uri.parse('http://localhost:3000/login');
-    final response = await http.post(url,
-        body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
+  // Get amount paid
+  stdout.write("Paid: ");
+  String? paid = stdin.readLineSync()?.trim();
 
-    if (response.statusCode == 200) {
-      final responseData = jsonDecode(response.body);
-      final userId = responseData[
-          'user_id']; // Now user_id is correctly retrieved from JSON
-      await showExpenses(userId);
-    } else if (response.statusCode == 401) {
-      print("Unauthorized: ${response.body}");
-    } else if (response.statusCode == 500) {
-      print("Server error: ${response.body}");
-    } else {
-      print("Unknown error: ${response.statusCode}");
-    }
+  if  (item == null || item.isEmpty || paid == null || paid.isEmpty) {
+  print("Please enter both an item and a valid amount.");
+  return;
   }
 
-  Future<void> showExpenses(int userId) async {
-    int? option;
+  int amountPaid;
+  try {
+    amountPaid = int.parse(paid);
+  } catch (e) {
+    print("Invalid amount entered.");
+    return;
+  }
 
-    do {
-      print("===== Expense Tracking App =====");
-      print("1. Show all");
-      print("2. Today's expense");
-      print("3. Exit");
+  // Prepare the data to send
+  final body = {
+    "user_id": userId,
+    "item": item,
+    "paid": amountPaid,
+    "date": DateTime.now().toUtc().toIso8601String() // Current date and time
+  };
 
-      stdout.write("Choose...");
+  // Send the request to the server
+  final url = Uri.parse('http://localhost:3000/add_expense');
+  final response = await http.post(url, body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
 
-      try {
-        option = int.parse(stdin.readLineSync()!);
-      } catch (e) {
-        print("Invalid input, please enter a number.");
-        option = null; // Assign null if the parsing fails
-        continue; // Go back to the start of the loop
-      }
+  if (response.statusCode == 200) {
+    print("Expense added successfully!");
+  } else {
+    print("Error adding expense: ${response.statusCode} ${response.body}");
+  }
+}
 
-      if (option == 1 || option == 2) {
-        final url = Uri.parse('http://localhost:3000/account?user_id=$userId');
-        final response = await http.get(url);
+Future<void> login() async {
+  print("===== Login =====");
+ 
+  // Get username and password
+  stdout.write("Username: ");
+  String? username = stdin.readLineSync()?.trim();
+  stdout.write("Password: ");
+  String? password = stdin.readLineSync()?.trim();
+ 
+  if (username == null || password == null) {
+    print("Incomplete input");
+    return;
+  }
 
-        if (response.statusCode == 200) {
-          final data = jsonDecode(response.body);
 
-          // Using the correct keys returned by the backend
-          final List<dynamic> allExpenses = data['allExpenses'];
-          final List<dynamic> todayExpenses = data['todayExpenses'];
+  final body = {"username": username, "password": password};
+  final url = Uri.parse('http://localhost:3000/login');
+  final response = await http.post(url, body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
 
-          if (option == 1) {
-            // Displaying all expenses
-            print("----------------- All expenses --------------");
-            for (final expense in allExpenses) {
-              final dt = DateTime.parse(expense['date']).toLocal();
-              print("${expense['item']} : ${expense['paid']}฿ @ $dt");
-            }
-            final totalExpenses = allExpenses.fold<int>(
-                0, (total, expense) => total + (expense['paid'] as int));
-            print("Total expenses: $totalExpenses฿");
-          } else if (option == 2) {
-            // Displaying today's expenses
-            print("-------------- Today's expenses -------------");
-            for (final expense in todayExpenses) {
-              final dt = DateTime.parse(expense['date']).toLocal();
-              print("${expense['item']} : ${expense['paid']}฿ @ $dt");
-            }
-            final totalTodayExpenses = todayExpenses.fold<int>(
-                0, (total, expense) => total + (expense['paid'] as int));
-            print("Total today's expenses: $totalTodayExpenses฿");
+
+  if (response.statusCode == 200) {
+    final responseData = jsonDecode(response.body);
+    final userId = responseData['user_id']; // Now user_id is correctly retrieved from JSON
+    await showExpenses(userId);
+  } else if (response.statusCode == 401) {
+    print("Unauthorized: ${response.body}");
+  } else if (response.statusCode == 500) {
+    print("Server error: ${response.body}");
+  } else {
+    print("Unknown error: ${response.statusCode}");
+  }
+}
+
+
+Future<void> showExpenses(int userId) async {
+  int? option;
+
+
+  do {
+    print("===== Expense Tracking App =====");
+    print("1. Show all");
+    print("2. Today's expense");
+    print("3. Search expense");
+    print("4. Add new expense");
+    print("5. Delete an expense");
+    print("6. Exit");
+
+
+    stdout.write("Choose...");
+
+
+    try {
+      option = int.parse(stdin.readLineSync()!);
+    } catch (e) {
+      print("Invalid input, please enter a number.");
+      option = null; // Assign null if the parsing fails
+      continue; // Go back to the start of the loop
+    }
+
+
+    if (option == 1 || option == 2) {
+      final url = Uri.parse('http://localhost:3000/account?user_id=$userId');
+      final response = await http.get(url);
+
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+
+
+        // Using the correct keys returned by the backend
+        final List<dynamic> allExpenses = data['allExpenses'];
+        // print(data['allExpenses']);
+        final List<dynamic> todayExpenses = data['todayExpenses'];
+
+
+        if (option == 1) {
+          // Displaying all expenses
+          print("----------------- All expenses --------------");
+          for (final expense in allExpenses) {
+            final dt = DateTime.parse(expense['date']).toLocal();
+            print("${expense['item']} : ${expense['paid']}฿ @ $dt");
           }
-        } else {
-          print("Error fetching expenses: ${response.statusCode}");
+          // final totalExpenses = allExpenses.fold<int>(0, (total, expense) => total + (expense['paid'] as int));
+          // print("Total expenses: $totalExpenses฿");
+        } else if (option == 2) {
+          // Displaying today's expenses
+          print("-------------- Today's expenses -------------");
+          for (final expense in todayExpenses) {
+            final dt = DateTime.parse(expense['date']).toLocal();
+            print("${expense['item']} : ${expense['paid']}฿ @ $dt");
+          }
+          final totalTodayExpenses = todayExpenses.fold<int>(0, (total, expense) => total + (expense['paid'] as int));
+          print("Total today's expenses: $totalTodayExpenses฿");
         }
-      } else if (option == 3) {
-        print("------ Bye --------");
       } else {
-        print("Invalid option. Please choose 1, 2, 3, 4, 5 or 6.");
+        print("Error fetching expenses: ${response.statusCode}");
       }
-    } while (option != 3); // The loop will continue until option 3 is selected
-  }
+    } else if(option == 3){
+     //////// Search
+      }
+      else if(option == 4){
+      // Add
+      await addNewExpense(userId);
+
+    }else if(option == 5){
+/////// Delete
+    }
+   
+   
+    else if (option == 6) {
+      print("------ Bye --------");
+    } else {
+      print("Invalid option. Please choose 1, 2, 3, 4, 5 or 6");
+    }
+  } while (option != 3); // The loop will continue until option 3 is selected
+
 }
--- SERVER/server_mobile_app.js
+++ SERVER/server_mobile_app.js
@@ -1,60 +1,107 @@
-const express = require('express');
-const app = express();
 const con = require("./db");
-const bcrypt = require('bcrypt');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-app.post('/login', (req, res)=>{
-    const {username, password} = req.body;
-    const sql = 'SELECT password FROM users WHERE username=?'
-    con.query(sql ,[username], (err, results)=>{
-        if(err){
-            return res.status(400).send('DB error');
-        } 
-        if(results.length != 1) {
-            return res.status(400).send('Wrong Password');
-        }
-        // compare raw and hash password
-        const hash = results[0].password;
-        bcrypt.compare(password, hash, (err, same)=>{
-            if(err){
-                return res.status(400).send('DB error');
-            } 
-            if(!same) {
-                return res.status(400).send('Login Failed');
-            }
-            res.send("Login OK");
-        })
+// password generator
+app.get("/password/:pass", (req, res) => {
+  const password = req.params.pass;
+  bcrypt.hash(password, 10, function (err, hash) {
+    if (err) {
+      return res.status(500).send("Hashing error");
+    }
+    res.send(hash);
+  });
+});
+
+// search
+app.post("/search", (req, res) => {
+  const { userid, itemsearch } = req.body;
+  // console.log('Received userid: ', userid);
+  const sql = "SELECT * FROM expense WHERE user_id = ? AND  item LIKE ?";
+
+  // console.log('Executing query:', sql, [userid, ]);
+  con.query(sql, [userid, `%${itemsearch}%`], (err, results) => {
+    if (err) {
+      return res.status(500).send("DB error");
+    }
+    res.json(results);
+    console.log(results);
+  });
+});
+
+// login
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const sql = "SELECT id, password FROM users WHERE username = ?";
+  con.query(sql, [username], function (err, results) {
+    if (err) {
+      return res.status(500).send("Database server error");
+    }
+    if (results.length != 1) {
+      return res.status(401).send("Wrong username");
+    }
+    // compare passwords
+    bcrypt.compare(password, results[0].password, function (err, same) {
+      if (err) {
+        return res.status(500).send("Hashing error");
+      }
+      if (same) {
+        return res.json({ message: "Login OK", user_id: results[0].id }); // Return user_id in JSON
+      }
+      return res.status(401).send("Wrong password");
     });
+  });
 });
 
-app.get('/password/:raw', (req, res)=>{
-    const raw = req.params.raw;
-    bcrypt.hash(raw, 10, (err, hash)=>{
-        if(err){
-            return res.status(500).send('Hash error')
-        }
-        else{
-            return res.send(hash);
-        }
-    })
-})
+app.get("/account", (req, res) => {
+  const userId = req.query.user_id; // Get user ID from query parameter
 
-app.get('/', (req, res) => {
-    res.send('hello');
-});
+  const sql = "SELECT * FROM expense WHERE user_id = ?";
+  con.query(sql, [userId], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Error fetching expenses");
+    }
 
-app.get('/expenses', (req, res) => {
-    const sql = 'SELECT * FROM expense';
-    con.query(sql, (err, results) => {
-        if (err) {
-            console.error(err); 
-            return res.status(500).send('DB error');
-        }
-        res.json(results);
+    // Filter expenses based on today's date
+    const today = new Date();
+    const todayExpenses = results.filter((expense) => {
+      const expenseDate = new Date(expense.date);
+      return expenseDate.toDateString() === today.toDateString();
     });
+    console.log(results);
+    res.json({
+      allExpenses: results,
+      todayExpenses: todayExpenses,
+    });
+  });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+// ADD
+// Endpoint to add a new expense
+app.post("/add_expense", async (req, res) => {
+  const { user_id, item, paid, date } = req.body;
+
+  const sql =
+    "INSERT INTO expense (user_id, item, paid, date) VALUES (?, ?, ?, ?);";
+
+  con.query(sql, [user_id, item, paid, date], function (err, results) {
+    if (err) {
+      return res.status(500).send("Database server error");
+    }
+    else{
+        return res.status(200).send("Ok");
+    }
+  });
+});
+
+// ---------- Server starts here ---------
+const port = 3000;
+app.listen(port, () => {
+  console.log("Server is running at " + port);
 });
