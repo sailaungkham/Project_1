88ac7e700dedbb0167eaf93fed8ae021a6c786e6
Srrt <seatsiri.d@gmail.com>
2024-08-22 08:48:42 +0700

    .

--- MOBILE/connection_demo/bin/connection_demo.dart
+++ MOBILE/connection_demo/bin/connection_demo.dart
@@ -1,6 +1,6 @@
 import 'package:connection_demo/connection_demo.dart' as connection_demo;
 import 'dart:convert';
 
-void main(List<String> arguments) {
-  print('Hello world: ${connection_demo.calculate()}!');
+void main() {
+  
 }
